c8d073ce870c687bfc3db32eda9339e07540d0b6
Kritika Roy <roykritika00@gmail.com>
2023-12-24 12:00:00 +0530

    citymoviesapp

---
+++ Ionic/CityMovies2/src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Swiper } from 'swiper';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  slideChanged(myslider: any) {
+    myslider.startAutoplay();
+  }
+  swiperSlideChanged(e: any) {
+    console.log('changed: ', e);
+  }
+
+  swiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  goNext() {
+    this.swiper?.slideNext();
+  }
+
+  goPrev() {
+    this.swiper?.slidePrev();
+  }
+}
